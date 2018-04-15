375049398839b04cfd1cace600192feec9485016
Dmitrii Shulyak <x350sh@gmail.com>
2018-04-16 00:03:21 +0300

    small change

--- hw2_1.py
+++ hw2_1.py
@@ -18,6 +18,7 @@ import os
 #     ]
 #   }
 
+
 def load_cook_book(file):
     cook_book = {}
     with open(file) as f:
@@ -36,19 +37,9 @@ def load_cook_book(file):
     return cook_book
 
 
-
-my_dir = os.path.dirname(__file__)
-my_file = "cook_book.txt"
-file_path = os.path.join(my_dir, my_file)
-
-# cook_book = load_cook_book('/home/x350/git/cook_book.txt')
-# print(cook_book["Омлет"])
-#
 def get_shop_list_by_dishes(dishes, person_count):
   shop_list = {}
   cook_book = load_cook_book(file_path)
-
-
   for dish in dishes:
     for ingridient in cook_book[dish]:
       new_shop_list_item = dict(ingridient)
@@ -60,10 +51,12 @@ def get_shop_list_by_dishes(dishes, person_count):
         shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
   return shop_list
 
+
 def print_shop_list(shop_list):
   for shop_list_item in shop_list.values():
     print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
                             shop_list_item['measure']))
+
 
 def create_shop_list():
   person_count = int(input('Введите количество человек: '))
@@ -72,4 +65,8 @@ def create_shop_list():
   shop_list = get_shop_list_by_dishes(dishes, person_count)
   print_shop_list(shop_list)
 
+
+my_dir = os.path.dirname(__file__)
+my_file = "cook_book.txt"
+file_path = os.path.join(my_dir, my_file)
 create_shop_list()
